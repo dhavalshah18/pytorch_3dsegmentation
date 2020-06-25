b1a76282d93b9d1be8092ab8511cca6e3d47eb3a
Dhaval Shah IBBMServer <dhaval@atnavab66.informatik.tu-muenchen.de>
2020-06-26 01:36:23 +0200

    Fixed dice loss

--- src/losses.py
+++ src/losses.py
@@ -6,36 +6,29 @@ import numpy as np
 
 
 def dice_loss(output, target):
-    """
-    input is a torch variable of size BatchxCxHxWxD representing log probabilities for each class
-    target is a 1-hot representation of the groundtruth, shoud have same size as the input
-    """
-    if len(target.size()) == 5:
-        target = target.squeeze(1)
     
-    # probs = B x C x H x W x D
-    probs = F.softmax(output, dim=1)
-        
-    # target = B x H x W x D
-    target = F.one_hot(target.long(), num_classes=2)
-    # target = B x C x H x W x D
+    if len(target.size()) == 5:
+        target = target.squeeze(1) 
+    
+    target = F.one_hot(target, num_classes=2)
     target = target.permute(0, 4, 1, 2, 3)
     
-    num = probs * target  # b,c,h,w--p*g
-    num = torch.sum(num, dim=3)  # b,c,h
-    num = torch.sum(num, dim=2)
+    pred = F.softmax(output, dim=1)
+    
+    dim = tuple(range(2, len(pred.size())))
+    
+    num = pred * target         # b,c,h,w,d
+    num = torch.sum(num, dim=dim) # b, c
+    
+    den1 = target**2
+    den1 = torch.sum(den1, dim=dim)
 
-    den1 = probs * probs  # --p^2
-    den1 = torch.sum(den1, dim=3)  # b,c,h
-    den1 = torch.sum(den1, dim=2)
+    den2 = pred**2
+    den2 = torch.sum(den2, dim=dim) 
+    
+    dice = 2*(num + 0.001)/(den1 + den2 + 0.001)
+    dice_eso = dice[:,1:]       # ignore background dice val, and take the foreground
 
-    den2 = target * target  # --g^2
-    den2 = torch.sum(den2, dim=3)  # b,c,h
-    den2 = torch.sum(den2, dim=2)  # b,c
-
-    dice = 2 * (num / (den1 + den2))
-    dice_eso = dice[:, 1:]  # we ignore bg dice val, and take the fg
-
-    dice_total = -1 * torch.sum(dice_eso) / dice_eso.size(0)  # divide by batch_sz
-
+    dice_total=-1*torch.sum(dice_eso)/dice_eso.size(0) # divide by batch_sz
+    
     return dice_total
