8f7b42177adb50bc2ccf9b84898e6998c0df3a03
Dhaval Shah IBBMServer <dhaval@atnavab66.informatik.tu-muenchen.de>
2020-06-29 18:21:43 +0200

    Changed save name for model

--- src/main.py
+++ src/main.py
@@ -15,18 +15,18 @@ def main():
 
     dataset_path = pathlib.Path("/home/dhaval/adam_data")
     
-    size = [28, 28, 28]
+    size = [20, 20, 20]
 
     train_data = MRAData(dataset_path, patch_size=size, mode="train")
     val_data = MRAData(dataset_path, patch_size=size, mode="val")
 
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=1, shuffle=True, num_workers=2)
-    val_loader = torch.utils.data.DataLoader(val_data, batch_size=1, shuffle=False, num_workers=2)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=5, shuffle=True, num_workers=2)
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=5, shuffle=False, num_workers=2)
         
     model = UNet(in_channels=1, out_channels=2, final_activation = "")
     model = model.cuda()
 
-    optim_args_SGD = {"lr": 1e-3, "weight_decay": 0.005, "momentum":0.9, "nesterov":True}
+    optim_args_SGD = {"lr": 2e-3, "weight_decay": 0.005, "momentum":0.9, "nesterov":True}
 
     optim_args_Adam = {"lr": 2e-2, "weight_decay": 0.005}
 
@@ -34,7 +34,7 @@ def main():
     
     solver.train(model, train_loader, val_loader, log_nth=5, num_epochs=50)
     
-    torch.save(model.state_dict(), "unet28.pth")
+    torch.save(model.state_dict(), "unet{0}.pth".format(size[0]))
     
 if __name__ == "__main__":
     main()
