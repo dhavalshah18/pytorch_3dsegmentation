948fa7fe037984a8d0b1eeed26f68ab269928f28
Dhaval Shah IBBMServer <dhaval@atnavab66.informatik.tu-muenchen.de>
2020-07-02 08:03:20 +0200

    Added file to store some data about trained models

--- src/main.py
+++ src/main.py
@@ -6,35 +6,43 @@ from data_utils import MRAData
 from trainer import Solver
 from blocks import *
 import os
-import nibabel as nib
+
 
 def main():
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1,2"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "3,5"
 
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
     dataset_path = pathlib.Path("/home/dhaval/adam_data")
     
-    size = [20, 20, 20]
+    size = [60, 60, 60]
+    batch_size = 5
+    optim = torch.optim.SGD
+    lr = 5e-2
+    wd = 0.000
+    epochs = 50
+    name = "unet{0}_{1}.pth".format(size[0], lr)
+
 
     train_data = MRAData(dataset_path, patch_size=size, mode="train")
     val_data = MRAData(dataset_path, patch_size=size, mode="val")
-
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=5, shuffle=True, num_workers=2)
-    val_loader = torch.utils.data.DataLoader(val_data, batch_size=5, shuffle=False, num_workers=2)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=2)
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=batch_size, shuffle=False, num_workers=2)
         
-    model = UNet(in_channels=1, out_channels=2, final_activation = "")
+    model = UNet(in_channels=1, out_channels=2, final_activation ="softmax")
     model = model.cuda()
-
-    optim_args_SGD = {"lr": 2e-3, "weight_decay": 0.005, "momentum":0.9, "nesterov":True}
-
-    optim_args_Adam = {"lr": 2e-2, "weight_decay": 0.005}
-
-    solver = Solver(optim_args=optim_args_SGD, optim=torch.optim.SGD)
     
-    solver.train(model, train_loader, val_loader, log_nth=5, num_epochs=50)
+    optim_args_SGD = {"lr": lr, "weight_decay": wd, "momentum":0.9, "nesterov":True}
+
+    solver = Solver(optim_args=optim_args_SGD, optim=optim)
     
-    torch.save(model.state_dict(), "unet{0}.pth".format(size[0]))
+    solver.train(model, train_loader, val_loader, log_nth=5, num_epochs=epochs)
     
+    torch.save(model.state_dict(), name)
+    
+    with open("saved_models.txt", "a") as savefile:
+        savefile.write("\n")
+        savefile.write("name={0}, patchsize={1}x{1}x{1}, batchsize={2}, optim={3}, lr={4}, wd={5}, epochs={6}".format(name, size[0], batch_size, optim, lr, wd, epochs))
+
 if __name__ == "__main__":
     main()
